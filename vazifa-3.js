59b96fa37099b1422af0893e9088697e08981457
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-06-02 11:48:17 +0500

    vazifa-3 done

--- vazifa-3.js
+++ vazifa-3.js
@@ -1,10 +1,14 @@
-let str = "abshihi";
+let str = "abshihihiih";
+let count = 0;
 
 countHi = (str) => {
-    let t = str.includes(str,0) == true;
-    if(t > 1){
-        return t;
+    for(let i = 0; i < str.length; i++){
+        if(str.substring(i,i+2) == 'hi'){
+            count ++;
+        }
     }
+
+    return count;
 }
 
 console.log(countHi(str));
